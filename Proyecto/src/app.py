5da728ee65737f67b13cb727343b5005d836f67d
karolin <karojg24@gmail.com>
2025-10-27 18:12:43 -0500

    feat: environment and docker files

---
+++ Proyecto/src/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/vamonos')
+def abajo():
+    return "vamonos"
+    
+@app.route('/')
+def metodo():
+    return "hola mundo"    
+    
+@app.route('/index')
+def index():
+    return render_template('index.html')
+    
+if __name__=='__main__':
+    app.run(host='0.0.0.0', port=8001)
